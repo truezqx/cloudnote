c67246a3be023b057502815bed558f4d6e31bb61
zqx <397524786@qq.com>
2017-07-08 19:40:03 +0800

    0708dao

--- src/main/java/cn/zqx/dao/ShareDao.java
+++ src/main/java/cn/zqx/dao/ShareDao.java
@@ -1,8 +1,13 @@
 package cn.zqx.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import cn.zqx.entity.Share;
 
 public interface ShareDao {
 	public int addShareNote(Share share);
-	public Share findById(String noteId);
+	public Share findByNoteId(String noteId);
+	public List<Share> findLikeTitle(Map<String,Object> params);
+	public Share findById(String shareId);
 }
